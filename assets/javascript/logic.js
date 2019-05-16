4f39f0a3ad52da74bf03c2e8445199374aefea97
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-16 16:11:40 -0500

    added bootstrap, basic game board and text styles

---
+++ assets/javascript/logic.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//     for (i=0; i < 1; i++){
+//         var buttons = $("<button>Add Button</button>");
+//         $(".tictactoe").append(buttons);
+//     };
+// });
--- assets/logic.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    for (i=0; i < 1; i++){
-        var buttons = $("<button>Add Button</button>");
-        $(".tictactoe").append(buttons);
-    };
-});

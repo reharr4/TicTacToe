5a31146fcfb28c056b1caf413eaedd7f43903bae
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-30 15:22:03 -0500

    working on js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,44 @@
+$( document ).ready(function() {
+
+
 // multiplayer 
+
+
+
+function xOSelector () {
+    var selectXO = "";
+    if (selectXO = true) {
+        $(".gamepiece").text("X");
+    } else {
+        $(".gamepiece").text("O");
+    }
+};
 
 //click on grid to allow player to select "X" or "O"
 
-//print selection to div  
+$(".gamepiece").click(function() {
+    selectXO = true; 
+    xOSelector ();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

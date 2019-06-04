4ce9eb7e4769e880bea0f53cab6bcc8e6b968dc7
Enragedtoast <edwardsjtimothy@gmail.com>
2019-06-03 20:44:24 -0500

    X O selection functional but clunky

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,35 +1,130 @@
 $( document ).ready(function() {
 
-
 // multiplayer 
 
 
 
-function xOSelector () {
-    if (selectXO = true) {
-        $(".gamepiece").text("X");
-    } else {
-        $(".gamepiece").text("O");
-    }
-};
+// variable to establish state of gamepiece
+
 
 //click on grid to allow player to select "X" or "O"
 
-$(".gamepiece").click(function() {
-    var clicks = $(this).data('clicks');
-  if (clicks) {
-    selectXO = true; 
-    xOSelector ();
-  } else {
-    selectXO = false; 
-    xOSelector ();
-  }
-  $(this).data("clicks", !clicks);
-});
+  $("#x1").click( function () {
+    var xOselector = Math.floor(Math.random() * 2); 
+    
+    
+    if (xOselector === 0) {
+    $("#x1").text("X");
+    xOselector = 1;
+    } else {
+      $("#x1").text("O");
+      xOselector = 0;
+    }
+  });
 
+  $("#x2").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#x2").text("X");
+    xOselector = 1;
+    } else {
+      $("#x2").text("O");
+      xOselector = 0;
+    }
+  });
+  
+  $("#x3").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#x3").text("X");
+    xOselector = 1;
+    } else {
+      $("#x3").text("O");
+      xOselector = 0;
+    }
+  });
 
+  $("#y1").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#y1").text("X");
+    xOselector = 1;
+    } else {
+      $("#y1").text("O");
+      xOselector = 0;
+    }
+  });
 
+  $("#y2").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#y2").text("X");
+    xOselector = 1;
+    } else {
+      $("#y2").text("O");
+      xOselector = 0;
+    }
+  });
 
+  $("#y3").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#y3").text("X");
+    xOselector = 1;
+    } else {
+      $("#y3").text("O");
+      xOselector = 0;
+    }
+  });
+
+  $("#z1").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#z1").text("X");
+    xOselector = 1;
+    } else {
+      $("#z1").text("O");
+      xOselector = 0;
+    }
+  });
+
+  $("#z2").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#z2").text("X");
+    xOselector = 1;
+    } else {
+      $("#z2").text("O");
+      xOselector = 0;
+    }
+  });
+
+  $("#z3").click( function () {
+    var xOselector = Math.floor(Math.random() * 2);
+    
+    
+    if (xOselector === 0) {
+    $("#z3").text("X");
+    xOselector = 1;
+    } else {
+      $("#z3").text("O");
+      xOselector = 0;
+    }
+  });
 
 
 
